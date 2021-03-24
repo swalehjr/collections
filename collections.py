83f19f653929ab74f253135b76411fada884bba1
swalehjr <77730709+swalehjr@users.noreply.github.com>
2021-03-24 15:18:07 +0300

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,19 @@
+fruits = ["orange","banana","cherry"]
+print(fruits)
+list1= [1,5,7,9,3]
+list2= [True,False,False]
+list3= ["apple",35,True,"john"]
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[0:2])
+fruits = ["apple" , "banana", "cherry"]
+if "apple" in fruits:
+  print("Yes,'apple' is in the frit list")
+fruits=["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+fruits[1]= "blackcurrent"
+print(fruits)
+fruits[1:3]=["watermelon"]
+print(fruits)
+fruits.insert(2,"passion")
+print(fruits)
